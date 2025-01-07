63d125dc9cbdab6a62a6858253c58b11f257ee66
cdghhhiilnnotu <nguyenthanhduong22072002@gmail.com>
2025-01-08 05:12:02 +0700

    post history

--- test_server.py
+++ test_server.py
@@ -8,14 +8,12 @@ import time
 def chat_llm(input):
     for chunk in chain.invoke(input):
         yield str(chunk)
-        # print(chunk)
-        history[-1]['assistant'] += chunk
 
-def chat_llm2(input):
-    res = ""
-    for chunk in chain.invoke(input):
-        res += str(chunk)
-    return res
+# def chat_llm2(input):
+#     res = ""
+#     for chunk in chain.invoke(input):
+#         res += str(chunk)
+#     return res
 
 # Now response the API
 app = FastAPI()
@@ -23,20 +21,17 @@ app = FastAPI()
 @app.post("/response/")
 async def get_response(data: dict):
     try:
-        history.append({
-            "user":data['query'],
-            "assistant":"",
-        })
+        history = data['history']
         return StreamingResponse(chat_llm(data['query']), media_type="text/plain; charset=utf-8")
     except FileNotFoundError:
         raise HTTPException(detail="File not found.", status_code=status.HTTP_404_NOT_FOUND)
     
-@app.post("/answer/")
-async def get_response(data: dict):
-    try:
-        return JSONResponse({"answer":chat_llm2(data['query'])})
-    except FileNotFoundError:
-        raise HTTPException(detail="File not found.", status_code=status.HTTP_404_NOT_FOUND)
+# @app.post("/answer/")
+# async def get_response(data: dict):
+#     try:
+#         return JSONResponse({"answer":chat_llm2(data['query'])})
+#     except FileNotFoundError:
+#         raise HTTPException(detail="File not found.", status_code=status.HTTP_404_NOT_FOUND)
     
 if __name__ == "__main__":
     uvicorn.run(app, host='127.0.0.1', port=1237)
