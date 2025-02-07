406c8c7db7e0f138f0696ce5a33da468738ad391
cdghhhiilnnotu <nguyenthanhduong22072002@gmail.com>
2025-02-07 14:27:33 +0700

    kaggle test

--- modules/tools/courses.py
+++ modules/tools/courses.py
@@ -5,6 +5,7 @@ from modules.configs import SCHEDULES_PATH, ACTIVE_COURSES_PATH
 
 @tool
 def course_fix(msv: str, maTC: str):
+    "Thay đổi học phần sẽ gây ảnh hưởng trực tiếp đến thời khóa biểu của sinh viên, hãy xác nhận là sinh viên muốn thực hiện."
     "Thay đổi học phần có mã tín chỉ maTC vào thời khóa biểu của sinh viên có mã sinh viên msv."
     print("Sử dụng tool Thay đổi học phần.")
 
@@ -32,6 +33,7 @@ def course_fix(msv: str, maTC: str):
 
 @tool
 def course_cancel(msv: str, maTC: str):
+    "Hủy học phần sẽ gây ảnh hưởng trực tiếp đến thời khóa biểu của sinh viên, hãy xác nhận là sinh viên muốn thực hiện."
     "Hủy học phần có mã tín chỉ maTC vào thời khóa biểu của sinh viên có mã sinh viên msv."
     print("Sử dụng tool Hủy học phần.")
     tkb = TKB(f'{SCHEDULES_PATH}/{msv}.csv')
