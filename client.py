a46c1d23296320ba85dabb7325f62c9e423ce60d
cdghhhiilnnotu <nguyenthanhduong22072002@gmail.com>
2025-02-10 16:46:53 +0700

    new samples router

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:1237/response/2055010153"
+url = "https://25d0-35-203-184-213.ngrok-free.app/response/2055010153"
 # url = "http://127.0.0.1:1237/login"
 
 def test_with(query=""):
@@ -34,13 +34,13 @@ def test_with(query=""):
                 print(line, end='')
 
 queries = [
-    "Chào bạn, tôi tên là Dương, tên bạn là gì?",
-    "Tên tôi là gì?",
-    "Học phần là gì?",
-    "Hủy đăng kí lớp học phần có mã tín chỉ TH4309 của sinh viên có mã sinh viên là 2055010153.",
-    "Đăng kí lớp học phần có mã tín chỉ TH4309 của sinh viên có mã sinh viên là 2055010051.",
-    "Tra cứu lịch thi của sinh viên có mã 2055010051 của ngày 03/02/2023.",
-    "Tra cứu lịch học của sinh viên có mã 2055010051 ngày 04/09/2023.",
+    # "Chào bạn, tôi tên là Dương, tên bạn là gì?",
+    # "Tên tôi là gì?",
+    # "Học phần là gì?",
+    "không xác nhận hủy đăng kí lớp học phần có mã tín chỉ TH4309.",
+    # "đăng kí lớp học phần có mã tín chỉ TH4309.",
+    # "Tra cứu lịch thi của sinh viên có mã 2055010051 của ngày 03/02/2023.",
+    # "Tra cứu lịch học của sinh viên có mã 2055010051 ngày 04/09/2023.",
 ]
 for query in queries:
     print(query)
--- tet.py
+++
@@ -1,5 +0,0 @@
-from modules.admins import import_users
-
-
-
-print(import_users('data.csv'))
